860019726a16d44a38d75d0e5bf1ff514f99df18
Ivny Metzker <ivnymetzker@gmail.com>
2023-08-06 19:05:41 -0300

    :dizzy: Feat: Validator
    starting the logic for the cpf validator, capturing the cpf and stripping the special characters

--- assets/js/main.js
+++ assets/js/main.js
@@ -28,7 +28,7 @@ btnMode.addEventListener("click", () => {
 })
 
 /* input cpf */
-const cpf = document.querySelector("#cpf");
+let cpf = document.querySelector("#cpf");
 
 cpf.addEventListener("keyup", () => {
   let value = cpf.value.replace(/[^0-9]/g, "").replace(/^([\d]{3})([\d]{3})?([\d]{3})?([\d]{2})?/, "$1.$2.$3-$4");
@@ -36,3 +36,31 @@ cpf.addEventListener("keyup", () => {
   cpf.value = value;
 });
 
+/* validation */
+const cpfTest = '131.776.926-01'
+
+let cpfClean = cpfTest.replace(/\D+/g, '');
+cpfArray = Array.from(cpfClean);
+console.log(cpfArray.reduce((ac, val) => ac + Number(val), 0));
+
+function ValidaCPF(cpfEnviado) {
+    Object.defineProperty(this, 'cpfClena', {
+      enumerable: true,
+      get: function() {
+        return cpfEnviado.replace(/\D+/g, '');
+      }
+    });
+  }
+  
+  ValidaCPF.prototype.valida = function() {
+    if(typeof this.cpfLimpo === 'undefined') return false;
+    if(this.cpfLimpo.length !== 11) return false;
+    if(this.isSequencia()) return false;
+  
+    const cpfParcial = this.cpfLimpo.slice(0, -2);
+    const digito1 = this.criaDigito(cpfParcial);
+    const digito2 = this.criaDigito(cpfParcial + digito1);
+  
+    const novoCpf = cpfParcial + digito1 + digito2;
+    return novoCpf === this.cpfLimpo;
+  };
